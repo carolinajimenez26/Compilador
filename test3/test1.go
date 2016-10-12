d5927dfdd37b4fd17e199b7262da06be5efc463f
enriqueangel <enriqe.angel.596@gmail.com>
2016-10-12 18:16:27 -0500

    Se modifica la prueba

--- test3/test1.go
+++ test3/test1.go
@@ -1,5 +1,24 @@
 var a int;
-var b int = 3;
+var b int;
 
-print a;
-print a + b;
+while true {
+
+    if a < b {
+        var c int;
+        while c > 0 {
+           c = c+1;
+        }
+    } else {
+        if a > 1 {
+            if a + 1 < c {
+               c = 0;
+            } else {
+               b = c - a;
+            }
+        }
+        while c - 3 == a {
+            a = b / c;
+            c = 3;
+        }
+    }
+}
